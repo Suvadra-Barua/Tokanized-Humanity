cd8b8ea43f1d9f299530dec08dbbced24e1ddb34
BanerjeeProdipta <64734379+BanerjeeProdipta@users.noreply.github.com>
2022-05-07 19:57:41 +0600

    feat: membership application

---
+++ src/pages/MembershipApplications.tsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+
+function MembershipApplications() {
+  return (
+    <div className="grid grid-cols-4 gap-6">
+      <div className="flex flex-col items-center p-6 border rounded-xl">
+        <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=461&q=80" alt="Bonnie" />
+        <h5 className="mb-1 text-xl font-medium text-center">Bonnie Green</h5>
+        <span className="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+        <div className="flex mt-4 space-x-3 lg:mt-6">
+          <Link
+            to="/applications/1"
+            className="px-4 py-1 text-white transition duration-500 rounded-full whitespace-nowrap bg-primary hover:bg-secondary"
+          >
+            View Application
+          </Link>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default MembershipApplications;

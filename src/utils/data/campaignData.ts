ce2d31c34cc5978767d56ac45e57858f9c8f5f92
BanerjeeProdipta <you@example.com>
2022-05-08 22:47:13 +0600

    fix: slider data

--- src/utils/data/campaignData.ts
+++ src/utils/data/campaignData.ts
@@ -3,24 +3,24 @@ import campaignCreatorData from './campaignCreatorData';
 const campaignData = [
   {
     id: 1,
-    name: 'Campaign 1',
+    name: 'Support for Praveen & Tamil Selvi family',
     description: 'We have been saddened with the news that Archie Low has been diagnosed with AML Leukemia and ha We have been saddened with the news that Archie Low',
-    startDate: '01/01/2020',
-    endDate: '01/01/2020',
+    startDate: '01/01/2022,
+    endDate: '01/05/2022',
     creatorData: campaignCreatorData[0],
-    fundRaised: '1000',
+    fundRaised: '800',
     fundGoal: '1500',
     donationCount: '6',
-    createdAt: '2h ago',
-    banner: 'https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
+    createdAt: '10 days ago',
+    banner: 'https://images.gofundme.com/Sleu7yTJfgsAonvd4wP92CVb5j4=/720x405/https://d2g8igdw686xgo.cloudfront.net/65057985_1651555085221672_r.jpeg',
     donations: [
       {
-        amount: 10,
+        amount: 200,
         donatedAt: '2 Days Ago',
         donator: campaignCreatorData[0],
       },
       {
-        amount: 5,
+        amount: 600,
         donatedAt: '2 Days Ago',
         donator: campaignCreatorData[2],
       },
@@ -28,13 +28,13 @@ const campaignData = [
     comments: [
       {
         id: 1,
-        comment: 'Comment 1',
+        comment: 'Keep us the good work',
         createdBy: campaignCreatorData[0],
         createdDate: '01/01/2020',
       },
       {
         id: 2,
-        comment: 'Comment 2',
+        comment: 'Good job',
         createdBy: campaignCreatorData[0],
         createdDate: '01/01/2020',
       },
@@ -42,16 +42,16 @@ const campaignData = [
   },
   {
     id: 2,
-    name: 'Campaign 1',
-    description: 'We have been saddened with the news that Archie Low has been diagnosed with AML Leukemia and ha We have been saddened with the news that Archie Low',
-    startDate: '01/01/2020',
-    endDate: '01/01/2020',
+    name: 'Help the Clifford Boys purchase their home',
+    description: 'Patrick 21, Jack 14, Andrew 8 and Conor 5 lost both their parents to cancer in the space of five months. This heartbreaking story has touched many people near and far, it has left the community of Milltown in disbelief that this could happen to one family in such a short space of time. In May 2021 the boys mom, Elaine,was diganosed with stage four cancer, after a short time and a big fight,she lost her battle to cancer on August 28th 2021, leaving behing her heartbroken husband Padraig and her beloved boys. Padraig  found it very difficult to come to terms with the loss of his soulmate, his amazing wife and mother to their four beautiful boys',
+    startDate: '03/02/2022',
+    endDate: '04/03/2022',
     creatorData: campaignCreatorData[1],
     fundRaised: '1000',
-    fundGoal: '1500',
+    fundGoal: '2000',
     donationCount: '6',
     createdAt: '2h ago',
-    banner: 'https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
+    banner: 'https://images.gofundme.com/tPE28jkw-g2P9DYL1dcFdYMfBRc=/720x405/https://d2g8igdw686xgo.cloudfront.net/64945289_1651072008563378_r.jpeg',
     donations: [
       {
         amount: 10,
@@ -67,13 +67,13 @@ const campaignData = [
     comments: [
       {
         id: 3,
-        comment: 'Comment 1',
+        comment: 'Great',
         createdBy: campaignCreatorData[0],
         createdDate: '01/01/2020',
       },
       {
         id: 4,
-        comment: 'Comment 2',
+        comment: 'We are with you',
         createdBy: campaignCreatorData[0],
         createdDate: '01/01/2020',
       },
@@ -81,16 +81,16 @@ const campaignData = [
   },
   {
     id: 3,
-    name: 'Campaign 1',
-    description: 'We have been saddened with the news that Archie Low has been diagnosed with AML Leukemia and ha We have been saddened with the news that Archie Low',
-    startDate: '01/01/2020',
-    endDate: '01/01/2020',
+    name: 'Emergency Support Fund for Thomas "Jay" James',
+    description: 'Thomas R James "Jay" served 32 years in prison for a crime he did not commit. For decades, Jay maintained his innocence while he diligently investigated his wrongful conviction and fought for his freedom.',
+    startDate: '08/02/2022',
+    endDate: '01/04/2022',
     creatorData: campaignCreatorData[2],
-    fundRaised: '1000',
-    fundGoal: '1500',
+    fundRaised: '1100',
+    fundGoal: '1000',
     donationCount: '6',
     createdAt: '2h ago',
-    banner: 'https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
+    banner: 'https://images.gofundme.com/PmoZjhns9_bRHGLbmG9E0RV_fd8=/720x405/https://d2g8igdw686xgo.cloudfront.net/64962161_1651122877833917_r.jpeg',
     donations: [
       {
         amount: 10,
@@ -106,13 +106,13 @@ const campaignData = [
     comments: [
       {
         id: 5,
-        comment: 'Comment 1',
+        comment: 'Go ahead!',
         createdBy: campaignCreatorData[0],
         createdDate: '01/01/2020',
       },
       {
         id: 6,
-        comment: 'Comment 2',
+        comment: 'Excellent work',
         createdBy: campaignCreatorData[0],
         createdDate: '01/01/2020',
       },
@@ -120,16 +120,16 @@ const campaignData = [
   },
   {
     id: 4,
-    name: 'Campaign 1',
-    description: 'We have been saddened with the news that Archie Low has been diagnosed with AML Leukemia and ha We have been saddened with the news that Archie Low',
-    startDate: '01/01/2020',
-    endDate: '01/01/2020',
+    name: 'Support Tekle Kiflemariam aka Wedi Tikul',
+    description: 'On behalf of the Eritrean community in diaspora we would like to bring to your attention that the renowned artist Tekle Kflemariam (Wedi Tkul) is in need of our support as he is diagnosed with an illness and is being taken care of in Italy. As we all know, Tekle, Wedi Tkul, gave most of his life and livelihood during the Eritrean liberation struggle and continued his heroic work until now. Through his art, Wedi Tkul strengthened the morale of Eritrean people and it’s combatants during liberation struggle and beyond.',
+    startDate: '04/01/2021',
+    endDate: '01/03/2021',
     creatorData: campaignCreatorData[3],
     fundRaised: '1000',
     fundGoal: '1500',
     donationCount: '6',
     createdAt: '2h ago',
-    banner: 'https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80',
+    banner: 'https://images.gofundme.com/z9kkankEZYoHl6aRwk-Ei08HQo0=/720x405/https://d2g8igdw686xgo.cloudfront.net/65122009_1651811751593908_r.png',
     donations: [
       {
         amount: 10,
@@ -145,13 +145,13 @@ const campaignData = [
     comments: [
       {
         id: 7,
-        comment: 'Comment 1',
+        comment: 'Bravo',
         createdBy: campaignCreatorData[0],
         createdDate: '01/01/2020',
       },
       {
         id: 8,
-        comment: 'Comment 2',
+        comment: 'I wish this becomes successful',
         createdBy: campaignCreatorData[0],
         createdDate: '01/01/2020',
       },

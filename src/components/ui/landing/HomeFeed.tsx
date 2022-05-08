bc471a14835d1725710b81c73f1008ab7b9358fe
BanerjeeProdipta <64734379+BanerjeeProdipta@users.noreply.github.com>
2022-05-08 15:54:49 +0600

    feat: homefeed

--- src/components/ui/landing/HomeFeed.tsx
+++ src/components/ui/landing/HomeFeed.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import campaignData from '../../../utils/data/campaignData';
 
 function HomeFeed() {
   return (
@@ -6,29 +7,34 @@ function HomeFeed() {
       <h2 className="pb-2 text-2xl font-bold">Updates</h2>
 
       {
-    [1, 2, 3, 4].map((index) => (
-      <div key={index} className="w-full py-2 mb-4">
-        <div className="flex ">
+
+    campaignData.map((campaign) => (
+      <div key={campaign.id} className="w-full py-2 mb-4">
+        <div className="flex">
           <div className="flex-shrink-0">
             <img
               className="rounded-full w-14 h-14"
-              src="https://lavinephotography.com.au/wp-content/uploads/2017/01/PROFILE-Photography-112.jpg"
+              src={campaign.creatorData.profilePicture}
               alt=""
             />
           </div>
           <div className="w-full ml-2 rounded-xl">
             <div className="text-sm">
               <Link
-                to="/user/profile"
+                to={`/user/${campaign.creatorData.id}`}
                 className="font-bold text-gray-800 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-300"
               >
-                Jhon Doe
+                {campaign.creatorData.name}
               </Link>
               <span className="mr-1 text-secondary">
-                &nbsp; @jhondoe
+                &nbsp;
+                {' '}
+                {campaign.creatorData.email}
               </span>
               <span className="mr-1 text-gray-600">
-                2h
+                {
+                campaign.createdAt
+              }
               </span>
             </div>
 
@@ -43,29 +49,24 @@ function HomeFeed() {
                   className="max-w-xs overflow-y-auto rounded-lg lg:w-full h-96"
                 />
               </div>
-              <div className="pt-4 space-y-4">
-                <div className="flex items-center rounded-full">
-                  <div className="flex flex-shrink-0">
-                    <img
-                      className="w-8 h-8 mr-2 rounded-full"
-                      src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
-                      alt=""
-                    />
-                  </div>
-                  <p className="text-sm opacity-80">blah blah</p>
-                </div>
+              {
+                campaign.comments.map((comment) => (
+                  <div key={comment.id} className="pt-4 space-y-4">
+                    <div className="flex items-center rounded-full">
+                      <div className="flex flex-shrink-0">
+                        <img
+                          className="w-8 h-8 mr-2 rounded-full"
+                          src={comment.createdBy.profilePicture}
+                          alt=""
+                        />
+                      </div>
+                      <p className="text-sm opacity-80">{comment.comment}</p>
+                    </div>
 
-                <div className="flex items-center rounded-full">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="w-8 h-8 mr-2 rounded-full"
-                      src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
-                      alt=""
-                    />
                   </div>
-                  <p className="text-sm opacity-70">blah blah</p>
-                </div>
-              </div>
+                ))
+              }
+
             </div>
           </div>
 

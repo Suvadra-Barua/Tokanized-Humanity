d9ac0f693aa1104a1553f8933977d1439ca39389
BanerjeeProdipta <64734379+BanerjeeProdipta@users.noreply.github.com>
2022-05-08 14:42:05 +0600

    fix: donator image and links

--- src/pages/FundRequestDetails.tsx
+++ src/pages/FundRequestDetails.tsx
@@ -113,7 +113,7 @@ function FundRequestDetails() {
                       {campaignData[params.id as unknown as number - 1].donations.map(
                         (donation, index) => (
                         // eslint-disable-next-line react/no-array-index-key
-                          <div key={index} className="flex items-center space-x-2">
+                          <Link to={`/user/${donation.donator.id}`} key={index} className="flex items-center space-x-2">
                             <img
                               src={donation.donator.profilePicture}
                               alt=""
@@ -138,7 +138,7 @@ function FundRequestDetails() {
                                 </p>
                               </div>
                             </div>
-                          </div>
+                          </Link>
                         ),
                       )}
 
@@ -161,7 +161,7 @@ function FundRequestDetails() {
          <Link to="/user/1" className="flex-shrink-0">
            <img
              className="inline-block w-12 h-12 mr-4 rounded-full"
-             src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
+             src={campaignData[params.id as unknown as number - 1].creatorData.profilePicture}
              alt=""
            />
          </Link>

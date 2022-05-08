e602777d4b0a71b4c0648fb0f30c8613e03d1815
BanerjeeProdipta <64734379+BanerjeeProdipta@users.noreply.github.com>
2022-05-08 14:31:31 +0600

    feat: slider

--- src/components/ui/landing/Slider.tsx
+++ src/components/ui/landing/Slider.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
+import campaignData from '../../../utils/data/campaignData';
 import settings from '../../../utils/slider';
 
 function SliderComponent() {
@@ -10,39 +11,40 @@ function SliderComponent() {
       <h2 className="pb-2 text-2xl font-bold">Fundraisers in your community</h2>
       <Slider {...settings} className="w-full">
         {
-          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((index) => (
-            <div
-              key={index}
-              className="p-2 transition duration-700 border-2 hover:bg-gray-500/5 hover:border-primary group rounded-xl"
-            >
-              <Link
-                className="space-y-1 "
-                to={`/fund-request/${index}`}
-              >
-                <img
-                  className="object-fill w-full h-48 transition duration-700 group-hover:scale-105 rounded-xl"
-                  src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
-                  alt=""
-                />
-                <h2 className="pt-2">Fundraiser For Archie</h2>
-                <p className="text-sm text-gray-600 truncate">
-                  We have been saddened with the news that Archie Low
-                  has been diagnosed with AML Leukemia and ha
-                  We have been saddened with the news that Archie Low
-                  has been diagnosed with AML Leukemia and ha
-                  We have been saddened with the news that Archie Low
-                  has been diagnosed with AML Leukemia and ha
-                </p>
-                <div className="text-xs font-bold">
-                  <span className="text-primary">
-                    $13,733 raised
-                  </span>
-                  ·
-                  114 donations
-                </div>
-              </Link>
-            </div>
-          ))
+         campaignData.map((campaign) => (
+           <div
+             key={campaign.id}
+             className="p-2 transition duration-700 border-2 hover:bg-gray-500/5 hover:border-primary group rounded-xl"
+           >
+             <Link
+               className="space-y-1 "
+               to={`/fund-request/${campaign.id}`}
+             >
+               <img
+                 className="object-fill w-full h-48 transition duration-700 group-hover:scale-105 rounded-xl"
+                 src={campaign.banner}
+                 alt=""
+               />
+               <h2 className="pt-2">{campaign.name}</h2>
+               <p className="text-sm text-gray-600 truncate">
+                 {
+                 campaign.description
+               }
+               </p>
+               <div className="text-xs font-bold">
+                 <span className="text-primary">
+                   {campaign.fundRaised}
+                   {' '}
+                   raised
+                 </span>
+                 ·
+                 {campaign.donationCount}
+                 {' '}
+                 donations
+               </div>
+             </Link>
+           </div>
+         ))
           }
       </Slider>
     </div>
